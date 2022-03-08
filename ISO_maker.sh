cb4b183af3dd643efebda3522851ba17fe211d21
Fabian Grant Petrus <fabse_utilities@pm.me>
2022-03-08 13:09:49 +0000

    Organizing the script

--- ISO_maker.sh
+++ ISO_maker.sh
@@ -7,32 +7,37 @@
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
-# Non-configurable part
+# Assigning parameters
 
-  if [[ "/etc/vconsole.conf" ]]; then
+  if [[ "/etc/vconsole.conf" ]] && [[ -z "$KEYMAP" ]]; then
     KEYMAP="$(</etc/vconsole.conf)" # Defaults to local keymap
     KEYMAP_sorted=${KEYMAP#*=}
   fi
-
   if [[ -z "$ANSWERFILE_path" ]]; then
     SCRIPT="startup.sh"
   else
     SCRIPT="startup_with_answerfile.sh"
   fi
 
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Checking and installing any missing dependencies
+
   if [[ "$(pacman -Qs opendoas)" ]] && [[ -z "$(pacman -Qs sudo)" ]]; then
     doas pacman --noconfirm -S sudo
     DELETE="true"
   fi
-
   if [[ -z "$(pacman -Qs artools)" ]]; then
     sudo pacman --noconfirm -S artools iso-profiles
   fi
 
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Cleaning conflicting folders
+
   if [[ -d "/home/$(whoami)/ISO" ]]; then
     rm -rf /home/$(whoami)/ISO
   fi
-
   if [[ -d "/home/$(whoami)/BUILDISO" ]]; then
     if [[ -d "/home/$(whoami)/BUILDISO/buildiso/base/artix/bootfs" ]]; then
       sudo umount -l /home/$(whoami)/BUILDISO/buildiso/base/artix/bootfs
@@ -43,11 +48,19 @@
     sudo rm -rf /home/$(whoami)/BUILDISO
   fi
 
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Copies configs and creates folders
+
   cp -rf artools /home/$(whoami)/.config
   cp -rf artools-workspace /home/$(whoami)
   mkdir /home/$(whoami)/{BUILDISO,ISO}
-  buildiso -p base -x
 
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Builds the filesystem and applies modifications
+
+  buildiso -p base -x
   sudo sed -i 's/--noclear/--autologin root --noclear/' /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/dinit.d/tty1
   sudo cp scripts/"$SCRIPT" /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/profile.d/startup.sh
   sudo chmod u+x /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/profile.d/startup.sh
@@ -58,10 +71,13 @@
     sudo cp "$ANSWERFILE_path" /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/script
   fi
 
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Continues building the ISO with deletion of build-folder
+
   buildiso -p base -sc
   buildiso -p base -bc
   buildiso -p base -zc
-
   sudo rm -rf /home/$(whoami)/BUILDISO
   if [[ "$DELETE" == "true" ]]; then
     doas pacman --noconfirm -Rns sudo
