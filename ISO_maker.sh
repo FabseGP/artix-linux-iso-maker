119421e8b898c5a71c1b4591a089ac130d74e7bd
Fabian Grant Petrus <fabse_utilities@pm.me>
2022-03-15 10:27:58 +0000

    Replaced wpa_supplicant with iwd

--- ISO_maker.sh
+++ ISO_maker.sh
@@ -4,14 +4,16 @@
 
   KEYMAP="" # Only relevant if /etc/vconsole.conf doesn't exist
   ANSWERFILE_path="" # e.g. /home/USERNAME/answerfile
+  WIFI_SSID_path="" # e.g. /home/USERNAME/HOMEBOX-24GHZ.psk, where "HOMEBOX-24GHZ" is your WIFI_SSID; 
+                    # uses iwd to autoconnect to wifi-network: https://wiki.archlinux.org/title/Iwd#Network_configuration
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 # Assigning parameters
 
   if [[ "/etc/vconsole.conf" ]] && [[ -z "$KEYMAP" ]]; then
-    KEYMAP_fetch="$(</etc/vconsole.conf)" # Defaults to local keymap
-    KEYMAP=${KEYMAP#*=}
+    KEYMAP="$(</etc/vconsole.conf)" # Defaults to local keymap
+    KEYMAP_sorted=${KEYMAP#*=}
   fi
   if [[ -z "$ANSWERFILE_path" ]]; then
     SCRIPT="startup.sh"
@@ -66,11 +68,17 @@
   sudo chmod u+x /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/profile.d/startup.sh
   sudo mkdir /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/script
   sudo cp scripts/keymap.sh /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/script
-  if [[ "$ANSWERFILE_path" ]]; then
-    sudo sed -i "3s/^/  KEYMAP=$KEYMAP\n/" /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/script/keymap.sh
-  fi
+  sudo sed -i "3s/^/  KEYMAP=$KEYMAP_sorted\n/" /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/script/keymap.sh
   if [[ "$ANSWERFILE_path" ]]; then
     sudo cp "$ANSWERFILE_path" /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/script
+  fi
+  sudo touch /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/NetworkManager/conf.d/wifi_backend.conf
+  cat << EOF | sudo tee -a /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/NetworkManager/conf.d/wifi_backend.conf > /dev/null
+[device]
+wifi.backend=iwd
+EOF
+  if [[ "$WIFI_SSID_path" ]]; then
+    sudo cp "$WIFI_SSID_path" /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/var/lib/iwd
   fi
 
 #----------------------------------------------------------------------------------------------------------------------------------
