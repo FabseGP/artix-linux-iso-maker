c8677b2c2e1256d39efbd38628568c4f2e6ec1f2
fabseman <fabse_utilities@pm.me>
2022-03-16 13:44:19 +0000

    Improved syntax

--- scripts/startup_with_answerfile.sh
+++ scripts/startup_with_answerfile.sh
@@ -1,17 +1,17 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 
-  cd /script
+  cd /script || exit
   openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -in .encrypt.txt -out .tmp.txt -pass file:.nothing.txt
   date | sha512sum > /.nothing/.nothing.txt
   openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -in .tmp.txt -out /.encrypt/.answer_encrypt.txt -pass file:/.nothing/.nothing.txt
   rm -rf {.encrypt.txt,.tmp.txt,.nothing.txt}
   ./keymap.sh
-  cd
+  cd || exit
   until ping -c 1 xkcd.com &> /dev/null; do
     nmtui
   done
   pacman -Sy
   git clone https://gitlab.com/FabseGP02/artix-install-script.git
-  cd artix-install-script
+  cd artix-install-script || exit
   chmod u+x install_artix.sh
   ./install_artix.sh
