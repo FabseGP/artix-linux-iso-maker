c37193b4ecbd602e368f454fb587f3620c5c9360
fabseman <fabse_utilities@pm.me>
2022-03-19 11:12:49 +0000

    Minor changes

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 pacman-key --init
-pacman -Syyu
+pacman -Syy
 pacman-key --populate
 pacman-key --refresh-keys
-pacman -Sy --noconfirm artix-keyring artix-archlinux-support
+pacman -Syy --noconfirm artix-keyring artix-archlinux-support
 pacman-key --init
 pacman-key --populate archlinux artix
 mv /pacman.conf /etc/pacman.conf
