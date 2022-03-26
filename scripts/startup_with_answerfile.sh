52fb6115788b235118f55c473745b5407a0e0e39
fabseman <fabse_utilities@pm.me>
2022-03-26 10:37:42 +0000

    Fixed encryption of answerfile

--- scripts/startup_with_answerfile.sh
+++ scripts/startup_with_answerfile.sh
@@ -2,10 +2,10 @@
 
   cd /script || exit
   chmod u+x keymap.sh
-  openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -in .encrypt.txt -out .tmp.txt -pass file:.nothing.txt
-  date | sha512sum > /.nothing/.nothing.txt
-  openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -in .tmp.txt -out /.encrypt/.answer_encrypt.txt -pass file:/.nothing/.nothing.txt
-  rm -rf {.encrypt.txt,.tmp.txt,.nothing.txt}
+  openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -in encrypt.txt -out tmp.txt -pass file:nothing.txt
+  date | sha512sum > /.nothing/nothing.txt
+  openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -in tmp.txt -out /.encrypt/answer_encrypt.txt -pass file:/.nothing/nothing.txt
+  rm -rf {encrypt.txt,tmp.txt,nothing.txt}
   ./keymap.sh
   cd || exit
   until ping -c 1 xkcd.com &> /dev/null; do
