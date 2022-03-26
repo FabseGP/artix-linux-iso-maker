2a6018e4f837484c76b8e71b499672d178395f60
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-03-26 17:22:03 +0000

    Fixed gpg-keys for repositories

--- scripts/startup_with_answerfile.sh
+++ scripts/startup_with_answerfile.sh
@@ -11,7 +11,10 @@
   until ping -c 1 xkcd.com &> /dev/null; do
     nmtui
   done
-  pacman -Sy
+  pacman-key --init
+  pacman-key --populate artix archlinux
+  pacman -Scc --noconfirm
+  pacman -Syy  
   git clone https://gitlab.com/FabseGP02/artix-install-script.git
   cd artix-install-script || exit
   chmod u+x install_artix.sh
