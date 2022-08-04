743753647a41717f81febed37bb33dece0eb7f72
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-08-04 10:00:44 +0000

    One ISO - multiple answefiles

---
+++ scripts/startup_choice.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+cd /scripts || exit
+PS3='Choose your mode: '
+modes=("INTERACTIVE INSTALL" "BASE INSTALL" "MINIMAL INSTALL" "FULL INSTALL" "EXIT TO LIVE ISO")
+select option in "${modes[@]}"; do
+    case $option in
+        "INTERACTIVE INSTALL")
+            ./startup.sh
+            exit
+            ;;
+        "BASE INSTALL")
+            ./startup_with_answerfile.sh BASE
+            exit
+            ;;
+        "MINIMAL INSTALL")
+            ./startup_with_answerfile.sh MINIMAL
+            exit
+            ;;
+	    "FULL INSTALL")
+            ./startup_with_answerfile.sh FULL
+            exit
+	        ;;
+	     "EXIT TO LIVE ISO")
+	        echo "User requested exit"
+	        exit
+	        ;;
+         *) echo "Invalid option \"$REPLY\"; I don't have time for this!";;
+    esac
+done
