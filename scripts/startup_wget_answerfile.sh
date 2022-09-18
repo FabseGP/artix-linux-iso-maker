8f3ef9ac33e36bf7c219650d8e1e7574ad0de36f
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-09-18 18:23:32 +0000

    Adding missing commands to script

--- scripts/startup_wget_answerfile.sh
+++ scripts/startup_wget_answerfile.sh
@@ -2,3 +2,17 @@
 
   read -rp "Direct path to raw file, e.g. \"https://gitlab.com/FabseGP02/artix-linux-iso-maker/-/raw/main/answerfile\": " PATH
   wget -O /.nothing/answerfile_wget -q $PATH
+  chmod u+x keymap.sh
+  ./keymap.sh
+  cd || exit
+  until ping -c 1 xkcd.com &> /dev/null; do
+    nmtui
+  done
+  pacman-key --init
+  pacman-key --populate artix archlinux
+  pacman -Scc --noconfirm
+  pacman -Syy  
+  git clone https://gitlab.com/FabseGP02/artix-install-script.git
+  cd artix-install-script || exit
+  chmod u+x install_artix.sh
+  ./install_artix.sh
