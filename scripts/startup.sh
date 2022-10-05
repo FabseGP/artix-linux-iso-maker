01eac62d526b0c02f46552eba9fbf510199f7bb9
fabseman <fabse_utilities@pm.me>
2022-10-05 12:53:03 +0200

    Better syntax

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/bash
 
-  cd /scripts || exit
-  ./keymap.sh
-  cd || exit
-  until ping -c 1 xkcd.com &> /dev/null; do
-    nmtui
-  done
-  pacman-key --init
-  pacman-key --populate artix archlinux
-  pacman -Scc --noconfirm
-  pacman -Sy    
-  git clone https://gitlab.com/FabseGP02/artix-install-script.git
-  cd artix-install-script || exit
-  ./install_artix.sh
+  cd /scripts || exit && ./keymap.sh && cd || exit
+  until ping -c 1 xkcd.com &> /dev/null; do nmtui; done
+  pacman-key --init && pacman-key --populate artix archlinux && pacman -Scc --noconfirm && pacman -Sy    
+  git clone https://gitlab.com/FabseGP02/artix-install-script.git && cd artix-install-script || exit && ./install_artix.sh
--- scripts/startup_choice.sh
+++ scripts/startup_choice.sh
@@ -26,8 +26,7 @@ select option in "${modes[@]}"; do
             break
 	    ;;
 	 "EXIT TO LIVE ISO")
-            ./keymap.sh
-            cd || exit
+            ./keymap.sh && cd || exit
 	    echo "User requested exit"
 	    break
 	    ;;
