e2089b0e46feb018499eae2eb551809c7138499a
fabseman <4969281-FabseGP02@users.noreply.gitlab.com>
2022-12-26 15:56:36 +0000

    Fixed errors

--- scripts/startup_wget_answerfile.sh
+++ scripts/startup_wget_answerfile.sh
@@ -3,7 +3,13 @@
   cd /scripts || exit
   read -rp "Direct path to raw file, e.g. \"https://gitlab.com/FabseGP02/artix-linux-iso-maker/-/raw/main/answerfile\": " PATH
   wget -O /.nothing/answerfile_wget -q "$PATH"
-  ./keymap.sh && cd || exit
+  ./keymap.sh
+  cd || exit
   until ping -c 1 xkcd.com &> /dev/null; do nmtui; done
-  pacman-key --init && pacman-key --populate artix archlinux && pacman -Scc --noconfirm && pacman -Sy  
-  git clone https://gitlab.com/FabseGP02/artix-install-script.git && cd artix-install-script || exit && ./install_artix.sh
+  pacman-key --init 
+  pacman-key --populate artix archlinux 
+  pacman -Scc --noconfirm 
+  pacman -Sy  
+  git clone https://gitlab.com/FabseGP02/artix-install-script.git 
+  cd artix-install-script || exit 
+  ./install_artix.sh
