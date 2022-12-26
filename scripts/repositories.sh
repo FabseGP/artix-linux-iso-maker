3a4a44b6263283f8626aed5c8d5e384de9bb50d1
fabseman <fabse_utilities@pm.me>
2022-12-26 15:24:01 +0000

    Fixed errors

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
   mv /pacman_without_arch.conf /etc/pacman.conf
-  pacman-key --init && pacman-key --populate && pacman-key --refresh-keys
+  pacman-key --init 
+  pacman-key --populate 
+  pacman-key --refresh-keys
   pacman -Sy --noconfirm artix-keyring artix-archlinux-support
-  pacman-key --init && pacman-key --populate archlinux artix
+  pacman-key --init
+  pacman-key --populate archlinux artix
   mv /pacman_with_arch.conf /etc/pacman.conf
