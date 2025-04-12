9c07469978545e4400c242b4a1d581d15c42d0af
FabseGP <fabse_utilities@pm.me>
2025-04-12 21:10:13 +0200

    Modernized default ISO

--- ISO_maker.sh
+++ ISO_maker.sh
@@ -2,11 +2,10 @@
 
 # Static parameters
 
-  BEGINNER_DIR=$(pwd)
   index=1
   today=$(date +"%Y_%m_%d-%T")
   shopt -s nullglob
-  ISO_path=$(echo /home/$(whoami)/ISO/base/*.iso)
+  ISO_path=$(echo /home/"$(whoami)"/ISO/base/*.iso)
   
 #----------------------------------------------------------------------------------------------------------------------------------
 
@@ -21,7 +20,7 @@
 
 # Updating pacman-config + backup of existing
 
-  if [[ "$(pacman -Qs opendoas)" ]] && ! [[ "$(pacman -Qs --color always "sudo" | grep "local" | grep "sudo ")" == "" ]]; then SU_COMMAND="doas"; 
+  if [[ "$(pacman -Qs opendoas)" ]] && ! [[ "$(pacman -Qs run some commands as root)" ]]; then SU_COMMAND="doas"; 
   else SU_COMMAND="sudo"; fi
   if [[ -z "$(pacman -Qs artix-archlinux-support)" ]]; then "$SU_COMMAND" pacman -Syy --noconfirm --needed artix-archlinux-support; "$SU_COMMAND" pacman-key --populate archlinux; fi
 
@@ -29,7 +28,7 @@
 
 # Assigning parameters
 
-  if [[ "/etc/vconsole.conf" ]] && [[ -z "$KEYMAP" ]]; then KEYMAP_system="$(</etc/vconsole.conf)"; KEYMAP=${KEYMAP_system#*=}; fi
+  if [[ -f "/etc/vconsole.conf" ]] && [[ -z "$KEYMAP" ]]; then KEYMAP_system="$(</etc/vconsole.conf)"; KEYMAP=${KEYMAP_system#*=}; fi
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
@@ -37,7 +36,7 @@
 
   if [[ "$SU_COMMAND" == "doas" ]]; then
     doas pacman --noconfirm -S sudo 
-    echo ""$USER" ALL=(ALL:ALL) NOPASSWD: ALL" | doas tee -a /etc/sudoers > /dev/null 
+    echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | doas tee -a /etc/sudoers > /dev/null 
     DELETE_sudo="true"
   fi
   if [[ -z "$(pacman -Qs openssl)" ]] && [[ "$ANSWERFILE_path_minimal" || "$ANSWERFILE_path_full" || "$ANSWERFILE_path_base" ]]; then sudo pacman --noconfirm --needed -S openssl; DELETE_openssl="true"; fi
@@ -47,11 +46,11 @@
 
 # Cleaning conflicting folders
 
-  if [[ "$ISO_path" ]]; then mv -f /home/$(whoami)/ISO/base /home/$(whoami)/ISO/base-"$today"; fi
+  if [[ "$ISO_path" ]]; then mv -f /home/"$(whoami)"/ISO/base /home/"$(whoami)"/ISO/base-"$today"; fi
   if [[ -d "/home/$(whoami)/BUILDISO" ]]; then
-    if [[ -d "/home/$(whoami)/BUILDISO/buildiso/base/artix/bootfs" ]]; then sudo umount -l /home/$(whoami)/BUILDISO/buildiso/base/artix/bootfs; fi
-    if [[ -d "/home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs" ]]; then sudo umount -l /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs; fi
-    sudo rm -rf /home/$(whoami)/BUILDISO
+    if [[ -d "/home/$(whoami)/BUILDISO/buildiso/base/artix/bootfs" ]]; then sudo umount -l /home/"$(whoami)"/BUILDISO/buildiso/base/artix/bootfs; fi
+    if [[ -d "/home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs" ]]; then sudo umount -l /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs; fi
+    sudo rm -rf /home/"$(whoami)"/BUILDISO
   fi
 
 
@@ -59,9 +58,9 @@
 
 # Copies configs and creates folders
 
-  cp -rf artools /home/$(whoami)/.config 
-  cp -rf artools-workspace /home/$(whoami) 
-  mkdir /home/$(whoami)/{BUILDISO,ISO}
+  cp -rf artools /home/"$(whoami)"/.config 
+  cp -rf artools-workspace /home/"$(whoami)" 
+  mkdir /home/"$(whoami)"/{BUILDISO,ISO}
   sudo sed -i 's/\/usr\/src\/linux\/version/\/usr\/src\/linux-zen\/version/' /usr/bin/buildiso
 
 #----------------------------------------------------------------------------------------------------------------------------------
@@ -70,26 +69,26 @@
 
   sudo modprobe loop 
   buildiso -p base -x
-  sudo sed -i 's/--noclear/--autologin root --noclear/' /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/dinit.d/tty1
-  sudo cp scripts/startup_choice.sh /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/profile.d/startup_choice.sh
-  sudo mkdir /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/{scripts,.nothing,.encrypt,.decrypt}
-  sudo cp scripts/{startup.sh,startup_with_answerfile.sh,startup_wget_answerfile.sh,keymap.sh} /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/scripts
-  sudo sed -i "3s/^/  KEYMAP=$KEYMAP\n/" /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/scripts/keymap.sh
+  sudo sed -i 's/--noclear/--autologin root --noclear/' /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/etc/dinit.d/tty1
+  sudo cp scripts/startup_choice.sh /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/etc/profile.d/startup_choice.sh
+  sudo mkdir /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/{scripts,.nothing,.encrypt,.decrypt}
+  sudo cp scripts/{startup.sh,startup_with_answerfile.sh,startup_wget_answerfile.sh,keymap.sh} /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/scripts
+  sudo sed -i "3s/^/  KEYMAP=$KEYMAP\n/" /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/scripts/keymap.sh
   for answerfile in $ANSWERFILE_path_base $ANSWERFILE_path_minimal $ANSWERFILE_path_full; do
-    if [[ "$answerfile" ]]; then
-      mkdir /home/$(whoami)/.nothing$index
-      date | sha512sum > /home/$(whoami)/.nothing$index/nothing$index.txt
-      openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -in "$ANSWERFILE_path_base" -out /home/$(whoami)/.nothing$index/encrypt$index.txt -pass file:/home/$(whoami)/.nothing$index/nothing$index.txt
-      sudo cp /home/$(whoami)/.nothing$index/{nothing$index.txt,encrypt$index.txt} /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/scripts
-      rm -rf /home/$(whoami)/.nothing$index
+    if [[ -f "$answerfile" ]]; then
+      mkdir /home/"$(whoami)"/.nothing$index
+      date | sha512sum > /home/"$(whoami)"/.nothing$index/nothing$index.txt
+      openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -in "$ANSWERFILE_path_base" -out /home/"$(whoami)"/.nothing$index/encrypt$index.txt -pass file:/home/"$(whoami)"/.nothing$index/nothing$index.txt
+      sudo cp /home/"$(whoami)"/.nothing$index/{nothing$index.txt,encrypt$index.txt} /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/scripts
+      rm -rf /home/"$(whoami)"/.nothing$index
       (( index++ )) || true
     else (( index++ )) || true; fi
   done
-  sudo cp configs/wifi_backend.conf /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/etc/NetworkManager/conf.d/wifi_backend.conf
-  sudo cp configs/{pacman_with_arch.conf,pacman_without_arch.conf} /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/
-  sudo cp scripts/repositories.sh /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/
-  artix-chroot /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/ /bin/bash -c "bash /repositories.sh"
-  sudo rm -rf /home/$(whoami)/BUILDISO/buildiso/base/artix/rootfs/repositories.sh
+  sudo cp configs/wifi_backend.conf /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/etc/NetworkManager/conf.d/wifi_backend.conf
+  sudo cp configs/{pacman_with_arch.conf,pacman_without_arch.conf} /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/
+  sudo cp scripts/repositories.sh /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/
+  artix-chroot /home/"$(whoami)/"BUILDISO/buildiso/base/artix/rootfs/ /bin/bash -c "bash /repositories.sh"
+  sudo rm -rf /home/"$(whoami)"/BUILDISO/buildiso/base/artix/rootfs/repositories.sh
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
@@ -98,7 +97,7 @@
   buildiso -p base -sc 
   buildiso -p base -bc 
   buildiso -p base -zc
-  sudo rm -rf /home/$(whoami)/BUILDISO
+  sudo rm -rf /home/"$(whoami)"/BUILDISO
   if [[ "$DELETE_openssl" == "true" ]]; then sudo pacman --noconfirm -Rns openssl; fi
   if [[ "$DELETE_sudo" == "true" ]]; then doas pacman --noconfirm -Rns sudo; fi
   echo
--- scripts/keymap.sh
+++ scripts/keymap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-  loadkeys $KEYMAP
+  loadkeys "$KEYMAP"
